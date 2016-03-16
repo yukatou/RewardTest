c1d07a406c13d34163c94f6404a017645b08a730
yukatou <yu.lets0226@gmail.com>
2016-03-16 12:43:49 +0900

    Initial Commit

---
+++ RewardTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RewardTest
+//
+//  Created by yukatou on 2016/03/16.
+//  Copyright © 2016年 yukatou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RewardTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RewardTest
+//
+//  Created by yukatou on 2016/03/16.
+//  Copyright © 2016年 yukatou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
